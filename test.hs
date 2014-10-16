ab94bae49a9f0e54bb977568675358c77d8be40f
ebratt <eric_bratt@yahoo.com>
2014-10-16 15:43:44 -0500

    finished hw1

--- test.hs
+++ test.hs
@@ -13,11 +13,13 @@ factorial n = product [1..n]
 average ns = div (sum ns) (length ns)
 
 --qsort takes a list of xs with a head x and sorts the elements in ascending order:
+{-
 qsort[] = []
 qsort(x:xs) = qsort smaller ++ [x] ++ qsort larger
               where
               	smaller = [a | a <- xs, a <= x]
               	larger = [b | b <- xs, b > x]
+-}
 
 -- Make a list of 0 to n:
 zeroto :: Int -> [Int]
@@ -26,3 +28,19 @@ zeroto n = [0..n]
 -- Mult takes three integers, one at a time, and returns their product:
 mult :: Int -> (Int -> (Int -> Int))
 mult x y z = x * y * z
+
+-- Week 1 Quiz:
+n = a `div` length xs
+  where a = 10
+  	    xs = [1,2,3,4,5]
+
+-- exercise 6:
+myProduct [] = 1
+myProduct (x : xs) = x * myProduct xs
+
+-- exercise 7:
+qsort [] = []
+qsort xs = x : qsort larger ++ qsort smaller
+  where x = maximum xs
+        smaller = [a | a <- xs, a < x]
+        larger = [b | b <- xs, b >= x]
