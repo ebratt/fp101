fef76081ca771a3b2603346d70b3e15592c9dab5
ebratt <eric_bratt@yahoo.com>
2014-10-16 14:47:54 -0500

    changed average to pre-fix versus in-fix

---
+++ test.hs
@@ -0,0 +1,28 @@
+-- setup some test data
+l1 = ['t','h','i','s',' ','i','s',' ','a',' ','l','i','s','t',' ','o','f',' ','c','h','a','r','s']
+l2 = [['a','b'],['c','d','e']]
+t1 = ("Yes", True, 'a', 1)
+
+-- Doubles an integer: 
+double x = x + x
+-- Quadruples an integer: 
+quadruple x = double (double x)
+-- Factorial of a positive integer:
+factorial n = product [1..n]
+-- Average of a list of integers:
+average ns = div (sum ns) (length ns)
+
+--qsort takes a list of xs with a head x and sorts the elements in ascending order:
+qsort[] = []
+qsort(x:xs) = qsort smaller ++ [x] ++ qsort larger
+              where
+              	smaller = [a | a <- xs, a <= x]
+              	larger = [b | b <- xs, b > x]
+
+-- Make a list of 0 to n:
+zeroto :: Int -> [Int]
+zeroto n = [0..n]
+
+-- Mult takes three integers, one at a time, and returns their product:
+mult :: Int -> (Int -> (Int -> Int))
+mult x y z = x * y * z
