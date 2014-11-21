c1db0a6bec276593f28e87fec6251f77b8f3f128
eric_bratt <eric_bratt@yahoo.com>
2014-11-21 09:48:51 -0600

    nothing big

--- week8.hs
+++ week8.hs
@@ -1,1 +1,14 @@
-week8.hs
+--week8.hs
+-- type IO () is similar to the "void" return type in java
+
+
+-- this function is purely imperative in that it does not return
+-- any value - rather, it simply has a side effect of printing to
+-- the console
+strlen :: IO ()
+strlen = do putStr "Enter a string: "
+            xs <- getLine      -- xs has a different type than getLine
+            putStr "The string has "
+            putStr (show (length xs))
+            putStrLn " characters"
+
